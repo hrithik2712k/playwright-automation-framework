2467000023b072ef3914ff4b512c9cae8affd61c
hrithik2712k <hrithik2712k@gmail.com>
2025-06-21 13:50:30 +0530

    changes_in_global_file

--- tests/ui-tests/global.setup.ts
+++ tests/ui-tests/global.setup.ts
@@ -9,9 +9,14 @@ test("Global Setup for Auto Login", async ({
 }) => {
   const decryptedUserName = commonUtils.decryptData(process.env.USER_NAME!);
   const decryptedPassword = commonUtils.decryptData(process.env.PASSWORD!);
+  console.log("Decrypted username length:", decryptedUserName.length);
+  console.log("Decrypted password length:", decryptedPassword.length);
 
   await loginPage.gotoOrangeHRM();
   await loginPage.loginOrangeHRM(decryptedUserName, decryptedPassword);
+  if (await loginPage.invalidCredentialErrorPopup.isVisible()) {
+    throw new Error("Login failed: Invalid credentials or login issue");
+  }
   await page.waitForURL(`${process.env.BASE_URL}web/index.php/dashboard/index`);
   await expect(dashboardPage.dashboardTitleText).toHaveText("Dashboard");
   await page.context().storageState({
